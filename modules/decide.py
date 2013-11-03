c187bdaf0d7492ec7832347036ccd6f75f00b6d6
Andri <crapmuffinz@gmail.com>
2013-11-04 01:48:26 +0200

    Create decide.py

---
+++ modules/decide.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Takes options separated by 'or' and returns one by random.
+# by andri
+from sys import argv
+from random import choice
+
+# Makes a list of all the options that are separated by " or "
+i = ' '.join(argv[3:]).split(' or ')
+# Returns a random option from the list
+print choice(i)
